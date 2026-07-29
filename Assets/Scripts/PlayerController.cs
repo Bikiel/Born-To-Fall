734d2559ffa9c066eff09c74908b47c5279b0e4a
josef7 <jose.f72000@gmail.com>
2026-07-29 18:52:08 -0500

    [ADD] Game over mechanic added

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -10,6 +10,7 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float acceleration = 25f;
 
     private Rigidbody2D playerRigidbody;
+    private bool isGameOver;
 
     private void Awake()
     {
@@ -28,6 +29,11 @@ public class PlayerController : MonoBehaviour
 
     private void FixedUpdate()
     {
+        if (isGameOver)
+        {
+            return;
+        }
+
         float horizontalInput = moveAction.ReadValue<Vector2>().x;
         float targetVelocityX = horizontalInput * moveSpeed;
         float velocityX = Mathf.MoveTowards(
@@ -36,5 +42,28 @@ public class PlayerController : MonoBehaviour
             acceleration * Time.fixedDeltaTime);
 
         playerRigidbody.linearVelocity = new Vector2(velocityX, playerRigidbody.linearVelocity.y);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            GameOver();
+        }
+    }
+
+    public void GameOver()
+    {
+        if (isGameOver)
+        {
+            return;
+        }
+
+        Debug.Log("Game Over!!");
+
+        isGameOver = true;
+        moveAction.Disable();
+        playerRigidbody.linearVelocity = Vector2.zero;
+        Time.timeScale = 0f;
     }
 }
