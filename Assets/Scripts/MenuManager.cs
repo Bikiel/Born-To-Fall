2c15a6d87d20f20e28920df695c0321e1c8a980e
josef7 <jose.f72000@gmail.com>
2026-07-30 10:33:09 -0500

    [ADD] Menu and credits screen implmentation added

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuManager : MonoBehaviour
+{
+    [SerializeField] private GameObject menuPanel;
+    [SerializeField] private GameObject creditsPanel;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+
+    public void ShowCredits()
+    {
+        menuPanel.SetActive(false);
+        creditsPanel.SetActive(true);
+    }
+
+    public void ShowMenu()
+    {
+        creditsPanel.SetActive(false);
+        menuPanel.SetActive(true);
+    }
+
+    public void ShowPlayScene()
+    {
+        SceneManager.LoadScene("Player");
+    }
+}
