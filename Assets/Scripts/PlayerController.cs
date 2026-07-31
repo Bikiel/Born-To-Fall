577a774a5d64c316990bbdab5d911332217637c3
josef7 <jose.f72000@gmail.com>
2026-07-31 09:30:53 -0500

    [ADD] First build

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -65,7 +65,6 @@ public class PlayerController : MonoBehaviour
 
     public void DisableMovement()
     {
-        Debug.Log("Game Over!!");
         moveAction.Disable();
         playerRigidbody.linearVelocity = Vector2.zero;
     }
